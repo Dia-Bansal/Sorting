6e325de637f4c6bc3ba54f010cb1b9d954d98423
Dia-Bansal <78194499+Dia-Bansal@users.noreply.github.com>
2022-05-21 18:04:47 +0530

    Print Bubble Sort Using Arrays

---
+++ BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+void BubbleSort(int arr[],int n){
+	for(int i=1;i<n;i++){
+		for(int j=0;j<n-i;j++){
+			if(arr[j]>arr[j+1]){
+				swap(arr[j],arr[j+1]);
+			}
+		}
+	}
+}
+
+int main(){
+	int size;
+	cin>>size;
+	int arr[100];
+	for(int i=0;i<size;i++){
+		cin>>arr[i];
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	BubbleSort(arr,size);
+	printArray(arr,size);
+}
